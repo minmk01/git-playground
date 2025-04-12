f6e179f869716455df237bb8fe30a2754d8bbaa7
Minkyung Lim <alsrudco1@gmail.com>
2025-04-12 22:50:54 +0900

    Project Setting

---
+++ git-playground/src/main/java/com/minn/git_playground/GitPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.minn.git_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPlaygroundApplication.class, args);
+	}
+
+}
---
+++ git-playground/src/test/java/com/minn/git_playground/GitPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minn.git_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
