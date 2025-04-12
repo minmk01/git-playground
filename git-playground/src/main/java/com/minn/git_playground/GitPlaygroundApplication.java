420920f05c973f6cc98004ff64f6a2c257f099ed
Minkyung Lim <alsrudco1@gmail.com>
2025-04-12 23:46:01 +0900

    Pull Request Test

--- git-playground/src/main/java/com/minn/git_playground/GitPlaygroundApplication.java
+++ git-playground/src/main/java/com/minn/git_playground/GitPlaygroundApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitPlaygroundApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GitPlaygroundApplication.class, args);
 	}
 
