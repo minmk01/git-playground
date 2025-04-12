56e0af7084db799278acb4e15c9dc93ea8f527b8
Minkyung Lim <alsrudco1@gmail.com>
2025-04-13 00:02:09 +0900

    글 생성 API

--- git-playground/src/main/java/com/minn/git_playground/TestController.java
+++ git-playground/src/main/java/com/minn/git_playground/TestController.java
@@ -1,5 +1,6 @@
 package com.minn.git_playground;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class TestController {
 
+    //글 생성
+    @PostMapping
 }
